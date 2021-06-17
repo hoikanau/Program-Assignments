b676cc6fb8b62ccb818cb5cc0444af5bc8879f5d
hoikanau <hoikanau123@gmail.com>
2021-06-17 11:42:13 -0700

    listPractice

---
+++ listPractice.py
@@ -0,0 +1,39 @@
+emptyList = []
+cityNames = [
+    "Oakland",
+    "Atlanta",
+    "New York",
+    "Seattle",
+    "Memphis",
+    "Miami",
+    "Boston",
+    "Los Angeles",
+    "Denver",
+    "New Orleans"
+]
+print(cityNames)
+print(cityNames[0], cityNames[2], cityNames[8])
+threeCities = cityNames[0:3]
+print(threeCities)
+cityNames[0] = "San Francisco"
+cityNames[2] = "Brooklyn"
+cityNames[7] = "Hollywood"
+cityNames[5] = "Tampa"
+print(cityNames[0], cityNames[2],cityNames[7],cityNames[5])
+
+colorNames = [
+    "Red",
+    "Blue",
+    "Yellow",
+    "Indigo",
+    "Purple",
+    "Green",
+    "Lime",
+    "Gold",
+    "Silver",
+    "Brown"
+]
+
+print(colorNames)
+threeColors = colorNames[4:7]
+print(threeColors)
