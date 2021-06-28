f8031a55fc6a3675b9fea57d24b8efe5b08626ef
hoikanau <hoikanau123@gmail.com>
2021-06-28 12:20:34 -0700

    6/28/2021 12:19

---
+++ loopCounter.py
@@ -0,0 +1,16 @@
+# loopCounter = const(9)
+# for x = 0 to loopCounter:
+#     repeat
+#         value = input("Enter a number between 1 and 10 inclusive")
+#     until value > 0 and value < 11
+#     print(calculate(value))
+# next x
+loopCounter = 19
+def loopValue(Times):
+    value = 1
+    for x in range (Times):
+        value = value + 1
+        print(value)
+    return value
+
+loopValue(loopCounter)
